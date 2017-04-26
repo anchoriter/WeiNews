3a2a773956720f5cc08ff607373c910f35c946ed
Anchoriter <781117967@qq.com>
2017-04-26 16:02:53 +0800

    初始化项目

---
+++ WeiNews/WeiNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiNews
+//
+//  Created by Anchoriter on 2017/4/26.
+//  Copyright © 2017年 Anchoriter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
